066c91320b52491f7b04147de580073e71c78404
philipwilson5 <philip.wilson@kainos.com>
2023-07-18 11:08:25 +0100

    feat(database): init commit

---
+++ scripts/DatabaseConstructor.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `JobRole` (
+`ID` SMALLINT unsigned AUTO_INCREMENT,
+`Name` VARCHAR(50),
+`Specification` VARCHAR(100),
+`BandLevel` VARCHAR(30),
+`Capability` VARCHAR(30),
+PRIMARY KEY (`ID`)
+);
+
+
+CREATE TABLE `Capability` (
+`ID` SMALLINT unsigned AUTO_INCREMENT,
+`Name` VARCHAR(50),
+`Capability` VARCHAR(50),
+PRIMARY KEY (`ID`)
+);
+
+
+CREATE TABLE `BandLevel` (
+`ID` SMALLINT unsigned AUTO_INCREMENT,
+`Name` VARCHAR(50),
+`BandLevel` VARCHAR(50),
+PRIMARY KEY (`ID`)
+);
+
+
---
+++ scripts/DatabaseInsert.sql
@@ -0,0 +1,11 @@
+INSERT INTO JobRole (ID ,Name ,Specification ,BandLevel,Capability) VALUES ('1', 'Joe Bloggs', 'Big Lad', 'Manager', 'Engineering');
+
+INSERT INTO JobRole (ID ,Name ,Specification ,BandLevel,Capability) VALUES ('2', 'Philip Wilson', 'Test Engineer', 'Placement', 'Digital Services');
+
+INSERT INTO Capability (ID, Name, Capability) VALUES ('1', 'Joe Bloggs', 'Engineering')
+
+INSERT INTO Capability (ID, Name, Capability) VALUES ('2', 'Philip Wilson', 'Digital Services')
+
+INSERT INTO BandLevel (ID, Name, BandLevel) VALUES ('1', 'Joe Bloggs', 'Manager')
+
+INSERT INTO BandLevel (ID, Name, BandLevel) VALUES ('2', 'Philip Wilson', 'Trainee')
