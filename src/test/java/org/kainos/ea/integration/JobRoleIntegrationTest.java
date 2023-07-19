b43bb1fff6fabfc3495e69f6b981acfa3f669f9e
Conn Meehan <conn.meehan@kainos.com>
2023-07-19 14:52:19 +0100

    feat(capability): created test classes

--- src/test/java/org/kainos/ea/integration/JobRoleIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/JobRoleIntegrationTest.java
@@ -1,30 +1,4 @@
 package org.kainos.ea.integration;
 
-import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
-import io.dropwizard.testing.junit5.DropwizardAppExtension;
-import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.kainos.ea.DropwizardWebServiceApplication;
-import org.kainos.ea.DropwizardWebServiceConfiguration;
-import org.kainos.ea.model.JobRole;
-
-import java.util.List;
-
-@ExtendWith(DropwizardExtensionsSupport.class)
 public class JobRoleIntegrationTest {
-    static final DropwizardAppExtension<DropwizardWebServiceConfiguration> APP = new DropwizardAppExtension<>(
-            DropwizardWebServiceApplication.class, null,
-            new ResourceConfigurationSourceProvider()
-    );
-
-    @Test
-    void test_getCapability_shouldReturnListOfCapabilities() {
-        List<JobRole> response = APP.client().target("http://localhost:8080/api/capability")
-                .request()
-                .get(List.class);
-
-        Assertions.assertTrue(response.size() > 0);
-    }
 }
