ff4ca1d316c4b4b52a52a2b3fb69e16bb693d97a
AlexMcC02 <alex.mccullough@kainos.com>
2023-07-20 09:28:52 +0100

    feat: database now uses env vars, github action updated

--- src/main/java/org/kainos/ea/util/DatabaseConnector.java
+++ src/main/java/org/kainos/ea/util/DatabaseConnector.java
@@ -1,39 +1,28 @@
 package org.kainos.ea.util;
 
-import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DatabaseConnector {
     private static Connection conn;
 
     public static Connection getConnection() throws SQLException {
-        String user, password, host, name;
+
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+        String host = System.getenv("DB_HOST");
+        String name = System.getenv("DB_NAME");
 
         if (conn != null && !conn.isClosed()) { return conn; }
 
-        try (FileInputStream propsStream = new FileInputStream("db.properties")) {
-            Properties props = new Properties();
-            props.load(propsStream);
-
-            user = props.getProperty("user");
-            password = props.getProperty("password");
-            host = props.getProperty("host");
-            name = props.getProperty("name");
-
-            if (user == null || password == null || host == null)
-                throw new IllegalArgumentException(("Properties file must exist " +
-                        "and must contain user, password, name and host properties."));
-
-            conn = DriverManager.getConnection("jdbc:mysql://" + host + "/" + name + "?useSSL=false", user, password);
+        try {
+            String connection = "jdbc:mysql://" + host + "/" + name + "?useSSL=false";
+            System.out.println("DEBUG: " + connection);
+            conn = DriverManager.getConnection(connection, user, password);
             return conn;
-
         } catch (Exception e) {
             System.err.println(e.getMessage());
-        } finally {
-            System.out.println("I will always run!");
         }
         return null;
     }
