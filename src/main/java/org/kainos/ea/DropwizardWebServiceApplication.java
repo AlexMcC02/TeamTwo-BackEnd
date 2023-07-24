1ae9a8a22407fe76af2a3e5f0d3be10b3b68ddd0
AlexMcC02 <137308751+AlexMcC02@users.noreply.github.com>
2023-07-24 11:26:12 +0100

    Revert "Revert "us001-view-job-roles""

--- src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
+++ src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
@@ -3,6 +3,12 @@ package org.kainos.ea;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+import org.kainos.ea.controller.JobRoleController;
+import org.kainos.ea.dao.JobRoleDao;
+import org.kainos.ea.service.JobRoleService;
+import org.kainos.ea.util.DatabaseConnector;
 
 public class DropwizardWebServiceApplication extends Application<DropwizardWebServiceConfiguration> {
 
@@ -17,13 +23,19 @@ public class DropwizardWebServiceApplication extends Application<DropwizardWebSe
 
     @Override
     public void initialize(final Bootstrap<DropwizardWebServiceConfiguration> bootstrap) {
-        // TODO: application initialization
+        bootstrap.addBundle(new SwaggerBundle<DropwizardWebServiceConfiguration>() {
+
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(DropwizardWebServiceConfiguration dropwizardWebServiceConfiguration) {
+                return dropwizardWebServiceConfiguration.getSwagger();
+            }
+        });
     }
 
     @Override
     public void run(final DropwizardWebServiceConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+        environment.jersey().register(new JobRoleController(new JobRoleService(new JobRoleDao(), new DatabaseConnector())));
     }
 
 }
--- src/main/java/org/kainos/ea/util/DatabaseConnector.java
+++ src/main/java/org/kainos/ea/util/DatabaseConnector.java
@@ -1,41 +1,30 @@
 package org.kainos.ea.util;
 
-import java.io.FileInputStream;
+import org.kainos.ea.exception.DatabaseConnectionException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DatabaseConnector {
     private static Connection conn;
 
-    public static Connection getConnection() throws SQLException {
-        String user, password, host, name;
+    public Connection getConnection() throws SQLException, DatabaseConnectionException {
+
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+        String host = System.getenv("DB_HOST");
+        String name = System.getenv("DB_NAME");
 
         if (conn != null && !conn.isClosed()) { return conn; }
 
-        try (FileInputStream propsStream = new FileInputStream("db.properties")) {
-            Properties props = new Properties();
-            props.load(propsStream);
-
-            user = props.getProperty("user");
-            password = props.getProperty("password");
-            host = props.getProperty("host");
-            name = props.getProperty("name");
-
-            if (user == null || password == null || host == null)
-                throw new IllegalArgumentException(("Properties file must exist " +
-                        "and must contain user, password, name and host properties."));
-
-            conn = DriverManager.getConnection("jdbc:mysql://" + host + "/" + name + "?useSSL=false", user, password);
+        try {
+            String connection = "jdbc:mysql://" + host + "/" + name + "?useSSL=false";
+            conn = DriverManager.getConnection(connection, user, password);
             return conn;
-
         } catch (Exception e) {
             System.err.println(e.getMessage());
-        } finally {
-            System.out.println("I will always run!");
+            throw new DatabaseConnectionException();
         }
-        return null;
     }
-
 }
