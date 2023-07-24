70ba0b643fea91cd679fb17f41b278fc9643790c
Conn Meehan <conn.meehan@kainos.com>
2023-07-24 14:25:29 +0100

    Apllication file changed

--- src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
+++ src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
@@ -6,33 +6,36 @@ import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 import org.kainos.ea.controller.JobRoleController;
+import org.kainos.ea.dao.JobRoleDao;
+import org.kainos.ea.service.JobRoleService;
+import org.kainos.ea.util.DatabaseConnector;
 
 public class DropwizardWebServiceApplication extends Application<DropwizardWebServiceConfiguration> {
 
-public static void main(final String[] args) throws Exception {
-new DropwizardWebServiceApplication().run(args);
-}
+    public static void main(final String[] args) throws Exception {
+        new DropwizardWebServiceApplication().run(args);
+    }
 
-@Override
-public String getName() {
-return "DropwizardWebService";
-}
+    @Override
+    public String getName() {
+        return "DropwizardWebService";
+    }
 
-@Override
-public void initialize(final Bootstrap<DropwizardWebServiceConfiguration> bootstrap) {
-bootstrap.addBundle(new SwaggerBundle<DropwizardWebServiceConfiguration>() {
+    @Override
+    public void initialize(final Bootstrap<DropwizardWebServiceConfiguration> bootstrap) {
+        bootstrap.addBundle(new SwaggerBundle<DropwizardWebServiceConfiguration>() {
 
-@Override
-protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(DropwizardWebServiceConfiguration dropwizardWebServiceConfiguration) {
-return dropwizardWebServiceConfiguration.getSwagger();
-}
-});
-}
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(DropwizardWebServiceConfiguration dropwizardWebServiceConfiguration) {
+                return dropwizardWebServiceConfiguration.getSwagger();
+            }
+        });
+    }
 
-@Override
-public void run(final DropwizardWebServiceConfiguration configuration,
-final Environment environment) {
-environment.jersey().register(new JobRoleController());
-}
+    @Override
+    public void run(final DropwizardWebServiceConfiguration configuration,
+                    final Environment environment) {
+        environment.jersey().register(new JobRoleController(new JobRoleService(new JobRoleDao(), new DatabaseConnector())));
+    }
 
 }
