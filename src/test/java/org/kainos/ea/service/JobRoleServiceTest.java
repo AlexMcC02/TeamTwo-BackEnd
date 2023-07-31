2cee79031406c370816ad263365add23118355b2
Kamil <kamil.biegaj@kainos.com>
2023-07-31 14:36:47 +0100

    Build: changes to pom file

--- src/test/java/org/kainos/ea/service/JobRoleServiceTest.java
+++ src/test/java/org/kainos/ea/service/JobRoleServiceTest.java
@@ -1,6 +1,5 @@
 package org.kainos.ea.service;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.dao.JobRoleDao;
